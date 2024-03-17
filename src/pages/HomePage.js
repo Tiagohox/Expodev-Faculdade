8e5037559fd27cc9cecdd3c54f3d52f41bd8325c
Tiago Henrique <128004411+Tiagohox@users.noreply.github.com>
2024-03-17 17:35:29 -0300

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomePage = () => {
+  
+  return (
+    
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Text> Puc Minas </Text>
+    </View>
+  );
+
+}
+
+export default HomePage;
